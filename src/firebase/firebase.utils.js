4bf050486cf618be0eb72256623560a365ce7094
Keenan (עזרא) <reaper31292@gmail.com>
2021-10-31 11:45:04 +0200

    added firestore database that saves Google logins

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
     measurementId: "G-8B4RSC7BHV"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists) {
+      const {displayName, email} = userAuth;
+      const dateCreated = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          dateCreated,
+          ...additionalData
+        })
+      } catch(err) {
+        console.log('error creating user', err)
+      }
+    }
+
+    return userRef;
+    console.log(snapShot);
+  }
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
