be25f959f376c76cd78d793c195986aedbe71905
Keenan (עזרא) <reaper31292@gmail.com>
2021-10-31 11:46:57 +0200

    added firestore database for Google logins (#7)

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -28,7 +28,7 @@ class App extends React.Component {
     auth.onAuthStateChanged(user => {
       this.setState({ currentUser: user });
 
-      console.log(user);
+      createUserProfileDocument(user);
     })
   }
 
