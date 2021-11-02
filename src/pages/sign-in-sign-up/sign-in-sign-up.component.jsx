177dbb1a3c8b7e50cc3490ea35d1bd2d8473255f
Keenan (עזרא) <reaper31292@gmail.com>
2021-11-02 23:47:14 +0200

    Normalized spacing across project

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -4,11 +4,11 @@ import './sign-in-sign-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
 
 const SignInSignUpPage = () => {
-    return(
-        <div className='sign-in-and-sign-up'>
-            <SignIn />
-        </div>
-    )
+	return(
+		<div className='sign-in-and-sign-up'>
+			<SignIn />
+		</div>
+	)
 }
 
 export default SignInSignUpPage;
